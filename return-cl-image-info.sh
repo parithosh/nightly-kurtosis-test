ce6f2b9b2fadcf76657fa63411778d86ef2da68b
parithosh <parithosh.jayanthi@ethereum.org>
2022-08-12 12:22:50 +0200

    updating versions

--- return-cl-image-info.sh
+++ return-cl-image-info.sh
@@ -7,10 +7,10 @@ echo $1:$IMAGE_NAME
 
 if [[ $1 == "teku" ]]
 then
-    echo "consensys/teku:develop"
+    echo "consensys/teku:22.8.0"
 elif [[ $1 == "lighthouse" ]]
 then
-    echo "sigp/lighthouse:latest-unstable"
+    echo "sigp/lighthouse:v2.5.1"
 elif [[ $1 == "nimbus" ]]
 then
     find_latest "parithoshj/nimbus"
@@ -19,17 +19,17 @@ then
     echo "chainsafe/lodestar:next"
 elif [[ $1 == "prysm" ]]
 then
-    echo "gcr.io/prysmaticlabs/prysm/beacon-chain:latest,gcr.io/prysmaticlabs/prysm/validator:latest"
+    echo "gcr.io/prysmaticlabs/prysm/beacon-chain:v2.1.4,gcr.io/prysmaticlabs/prysm/validator:v2.1.4"
 elif [[ $1 == "nethermind" ]]
 then
-    echo "nethermindeth/hive:latest"
+    echo "nethermindeth/nethermind:goerli_testing"
 elif [[ $1 == "besu" ]]
 then
-    echo "hyperledger/besu:develop"
+    echo "hyperledger/besu:22.7.1-SNAPSHOT-openjdk-11-debug-amd64"
 elif [[ $1 == "erigon" ]]
 then
     echo "parithoshj/erigon:devel"
 elif [[ $1 == "kurtosis" ]]
 then
-    echo "parithoshj/kurtosis:fix-bootnode-x86"
+    echo "kurtosistech/eth2-merge-kurtosis-module:0.5.7"
 fi
